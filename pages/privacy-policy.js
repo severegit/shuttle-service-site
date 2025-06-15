d6e6fd769257670e02bfd02d0d8a4aad619ae671
David Severe <davidsevere@Davids-MacBook-Air.local>
2025-06-14 20:09:29 -0400

    Added Privacy Policy page and updated design/theme

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -6,16 +6,86 @@ export default function PrivacyPolicy() {
       <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
       <p className="mb-4">
         This Privacy Policy ("Policy") applies to https://www.avemariashuttle.com, and SUSU SEVERE CARS, LLC ("Company") and governs data collection and usage. By using the Company website, you consent to the data practices described in this statement.
-      </p>
-      <p className="mb-2 font-semibold">Collection of your Personal Information</p>
-      <ul className="list-disc list-inside mb-4">
-        <li>First and last name</li>
-        <li>Mailing address</li>
-        <li>Email address</li>
-        <li>Phone number</li>
-      </ul>
-      <p className="mb-4">... (Truncated for brevity. Full policy content will be displayed here.)</p>
-      <p className="mt-8 text-sm text-gray-600">
+     Collection of your Personal Information 
+In order to better provide you with products and services offered, the Company may collect personally identifiable information, such as your: 
+  
+ -	First and last name 
+ -	Mailing address 
+ -	Email address 
+ -	Phone number 
+  
+We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services. These may include: (a) registering for an account; (b) entering a sweepstakes or contest sponsored by us or one of our partners; (c) signing up for special offers from selected third parties; (d) sending us an email message; (e) submitting your credit card or other payment information when ordering and purchasing products and services. To wit, we will use your information for, but not limited to, communicating with you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal information in the future. 
+  
+Use of your Personal Information 
+The Company collects and uses your personal information in the following ways:  
+  
+ -	to operate and deliver the services you have requested 
+ -	to provide you with information, products, or services that you request from us 
+ -	to provide you with notices about your account 
+ -	to carry out the Company's obligations and enforce our rights arising from any contracts entered between you and us, including for billing and collection 
+ -	to notify you about changes to our https://www.avemariashuttle.com or any products or services we offer or provide through it 
+ -	in any other way we may describe when you provide the information 
+ -	for any other purpose with your consent. 
+  
+The Company may also use your personally identifiable information to inform you of other products or services available from the Company and its affiliates. 
+  
+Sharing Information with Third Parties 
+The Company does not sell, rent, or lease its customer lists to third parties. 
+  
+The Company may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services tothe Company, and they are required to maintain the confidentiality of your information. 
+  
+The Company may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on the Company or the site; (b) protect and defend the rights or property of the Company; and/or (c) act under exigent circumstances to protect the personal safety of users of the Company, or the public. 
+  
+Tracking User Behavior 
+The Company may keep track of the websites and pages our users visit within the Company, in order to determine what the Company services are the most popular. This data is used to deliver customized content and advertising within the Company to customers whose behavior indicates that they are interested in a particular subject area. 
+  
+Automatically Collected Information 
+The Company may automatically collect information about your computer hardware and software. This information can include your IP address, browser type, domain names, access times, and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding the use of the Company's website. 
+  
+Links 
+This website contains links to other sites. Please be aware that we are not responsible for the content or privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of any other site that collects personally identifiable information. 
+  
+Security of your Personal Information 
+The Company secures your personal information from unauthorized access, use, or disclosure. The Company uses the following methods for this purpose: 
+  
+ -	SSL Protocol 
+  
+When personal information (such as a credit card number) is transmitted to other websites, it is protected through the use of encryption, such as the Secure Sockets Layer (SSL) protocol. 
+  
+We strive to take appropriate security measures to protect against unauthorized access to or alteration of your personal information. Unfortunately, no data transmission over the Internet or any wireless network can be guaranteed to be 100% secure. As a result, while we strive to protect your personal information, you acknowledge that: (a) there are security and privacy limitations inherent to the Internet that are beyond our control; and (b) the security, integrity, and privacy of any and all information and data exchanged between you and us through this site cannot be guaranteed. 
+  
+Right to Deletion 
+Subject to certain exceptions set out below, on receipt of a verifiable request from you, we will: 
+  
+ -	Delete your personal information from our records; and 
+ -	Direct any service providers to delete your personal information from their records. 
+  
+Please note that we may not be able to comply with requests to delete your personal information if it is necessary to: 
+  
+ -	Complete the transaction for which the personal information was collected, fulfill the terms of a written warranty or product recall conducted in accordance with federal law, and provide a good or service requested by you, or reasonably anticipated within the context of our ongoing business relationship with you, or otherwise perform a contract between you and us; 
+ -	Detect security incidents, protect against malicious, deceptive, fraudulent, or illegal activity; or prosecute those responsible for that activity; 
+ -	Debug to identify and repair errors that impair existing intended functionality; 
+ -	Exercise free speech, ensure the right of another consumer to exercise his or her right of free speech, or exercise another right provided for by law; 
+ -	Comply with the California Electronic Communications Privacy Act; 
+ -	Engage in public or peer-reviewed scientific, historical, or statistical research in the public interest that adheres to all other applicable ethics and privacy laws, when our deletion of the information is likely to render impossible or seriously impair the achievement of such research, provided we have obtained your informed consent; 
+ -	Enable solely internal uses that are reasonably aligned with your expectations based on your relationship with us; 
+ -	Comply with an existing legal obligation; or 
+ -	Otherwise use your personal information, internally, in a lawful manner that is compatible with the context in which you provided the information. 
+  
+Children Under Thirteen 
+The Company does not knowingly collect personally identifiable information from children under the age of 13. If you are under the age of 13, you must ask your parent or guardian for permission to use this website. 
+  
+Email Communications 
+From time to time, the Company may contact you via email for the purpose of providing announcements, promotional offers, alerts, confirmations, surveys, and/or other general communication. 
+  
+If you would like to stop receiving marketing or promotional communications via email from the Company, you may opt out of such communications by clicking unsubscribe.. 
+  
+Changes to This Statement 
+The Company reserves the right to change this Policy from time to time. For example, when there are changes in our services, changes in our data protection practices, or changes in the law. When changes to this Policy are significant, we will inform you. You may receive a notice by sending an email to the primary email address specified in your account, by placing a prominent notice on our SUSU SEVERE CARS, LLC, and/or by updating any privacy information. Your continued use of the website and/or services available after such modifications will constitute your: (a) acknowledgment of the modified Policy; and (b) agreement to abide and be bound by that Policy. 
+  
+Contact Information 
+The Company welcomes your questions or comments regarding this Policy. If you believe that the Company has not adhered to this Policy, please contact the Company at: 
+
         SUSU SEVERE CARS, LLC<br />
         1175 NE 125TH ST, 214<br />
         Miami, Florida 33161<br />
